cdc257a96fd05feb2cb937ede2734adf246d91ce
Ramesh Verma <rsverma333@gmail.com>
2023-07-02 21:14:01 +0530

    Added prototype pattern

---
+++ DemoApp/Prototype/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DemoApp
+{
+    public class Person : ICloneable
+    {
+        public int Age { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Name { get; set; }
+
+        public Address Address { get; set; }
+
+        public object Clone()
+        {
+            //Shallow Clone
+            return MemberwiseClone();
+        }
+        public object DeepClone()
+        {
+            return new Person { Age = Age, BirthDate = BirthDate, Name = Name, Address = new Address(Address.HouseNumber) };
+        }
+
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}, BirthDate: {BirthDate}, HouseNumber: {Address?.HouseNumber}";
+        }
+    }
+
+    public class Address
+    {
+        public int HouseNumber { get; set; }
+        public Address(int houseNumber)
+        {
+            this.HouseNumber = houseNumber;
+        }
+    }
+}
