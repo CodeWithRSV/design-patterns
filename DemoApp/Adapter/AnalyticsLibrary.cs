0ee01fb576aae238ae275663f4dfb5aeef1fa432
Ramesh Verma <rsverma333@gmail.com>
2023-07-03 19:57:40 +0530

    Adapter pattern

---
+++ DemoApp/Adapter/AnalyticsLibrary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DemoApp
+{
+    public class AnalyticsLibrary
+    {
+        public void DisplayGraph(CustomLibraryObject obj)
+        {
+            Console.WriteLine("Display graphs based on received custom object");
+        }
+    }
+
+    public class CustomLibraryObject
+    {
+    }
+}
---
+++ DemoApp/Adapter/IDatavisualizer.cs
@@ -0,0 +1,36 @@
+﻿/* Usage in main
+  
+            string JsonData = "Randon Json Data";
+            AnalyticsLibrary adaptee = new AnalyticsLibrary();
+            DataFormatAdapter adapter =  new DataFormatAdapter(adaptee);
+            adapter.DisplayGraph(JsonData);
+*/
+
+namespace DemoApp
+{
+    public interface IDatavisualizer
+    {
+        void DisplayGraph(string jsonData);
+    }
+    public class DataFormatAdapter : IDatavisualizer
+    {
+        private readonly AnalyticsLibrary _adaptee;
+
+        public DataFormatAdapter(AnalyticsLibrary adaptee)
+        {
+            _adaptee = adaptee;
+        }
+
+        public void DisplayGraph(string jsonData)
+        {
+            CustomLibraryObject obj = GetObjFromJSON(jsonData);
+            _adaptee.DisplayGraph(obj);
+        }
+
+        private CustomLibraryObject GetObjFromJSON(string jsonData)
+        {
+            //Convert json data to library object
+            return new CustomLibraryObject();
+        }
+    }
+}
