99afe2eda4973b509d9ac4d5501b8bfc97123271
Ramesh Verma <rsverma333@gmail.com>
2023-07-26 17:59:58 +0530

    Added decorator pattern

---
+++ DemoApp/Decorator/Pizza.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace DemoApp
+{
+    public interface IPizza
+    {
+        void Prepare();
+    }
+
+    class BasePizza : IPizza
+    {
+        public void Prepare()
+        {
+            Console.WriteLine("Pizza prepared");
+        }
+    }
+    public abstract class PizzaDecorator : IPizza
+    {
+        private readonly IPizza _pizza;
+
+        protected PizzaDecorator(IPizza pizza)
+        {
+            _pizza = pizza;
+        }
+
+        public virtual void Prepare()
+        {
+            _pizza.Prepare();
+        }
+    }
+    public class PepperoniPizza : PizzaDecorator
+    {
+        public PepperoniPizza(IPizza pizza) : base(pizza)
+        {
+        }
+        public override void Prepare()
+        {
+            base.Prepare();
+            Console.WriteLine("Adding Pepperoni Topping");
+        }
+    }
+    public class OnionPizza : PizzaDecorator
+    {
+        public OnionPizza(IPizza pizza) : base(pizza)
+        {
+        }
+        public override void Prepare()
+        {
+            base.Prepare();
+            Console.WriteLine("Adding Onion Topping");
+        }
+    }
+    public class CapsicumPizza : PizzaDecorator
+    {
+        public CapsicumPizza(IPizza pizza) : base(pizza)
+        {
+        }
+        public override void Prepare()
+        {
+            base.Prepare();
+            Console.WriteLine("Adding Capsicum Topping");
+        }
+    }
+}
